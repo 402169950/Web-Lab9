c1448871ab364ee71f56f5708c2b030684fdca99
Tong Liu <tliu77@myseneca.ca>
2021-04-10 23:06:20 +0800

    Change the DOM element to h2 to make each components looking more distinctive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
+/**
+ * URL: https://stupefied-curie-1889cd.netlify.app
+ *
+ *
+ *
+ */
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lab9-app';
